1bd143b0e50099c053829e4f5eb7f643872669dc
less-nik13 <62059273+less-nik13@users.noreply.github.com>
2021-10-31 19:57:20 +0200

    fix

--- hw_1/src/components/Form.jsx
+++ hw_1/src/components/Form.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import SubmitButton from "./SubmitButton";
 import Input from "./Input";
 
-
 class Form extends Component {
     constructor(props) {
         super(props);
@@ -18,26 +17,26 @@ class Form extends Component {
 
     handleChange(event) {
         const value = event.target.value;
+        const name = event.target.name;
         this.setState({
-            ...this.state, [event.target.name]: value
+            ...this.state, [name]: value
         });
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        if(!this.state.name || !this.state.email || !this.state.password) {
+
+        const { name, email, password } = this.state;
+        const userData = { name, email, password };
+
+        if(!name || !email || !password) {
             this.setState({
                 ...this.state, message: "All fields are required."
-            })
+            });
             return;
         }
 
-        if(!localStorage.getItem('userData')) {
-            localStorage.setItem('userData', JSON.stringify(this.state));
-        } else {
-            localStorage.clear();
-            localStorage.setItem('userData', JSON.stringify(this.state));
-        }
+        localStorage.setItem('userData', JSON.stringify(userData));
 
         this.setState({
             name: "",
@@ -48,15 +47,17 @@ class Form extends Component {
     }
 
     render() {
+        const {message, name, email, password} = this.state;
+
         return (
             <form className="form">
                 <h2>Task 1: Register</h2>
-                {localStorage.getItem("userData") && <h3 className="form__message">{this.state.message}</h3>}
-                <Input name="name" type="text" handleChange={this.handleChange} value={this.state.name}
+                {message && <h3 className="form__message">{message}</h3>}
+                <Input name="name" type="text" handleChange={this.handleChange} value={name}
                        maxLength="20"/>
-                <Input name="email" type="email" handleChange={this.handleChange} value={this.state.email}
+                <Input name="email" type="email" handleChange={this.handleChange} value={email}
                        maxLength="60"/>
-                <Input name="password" type="password" handleChange={this.handleChange} value={this.state.password}
+                <Input name="password" type="password" handleChange={this.handleChange} value={password}
                        maxLength="20"/>
                 <SubmitButton handleSubmit={this.handleSubmit}/>
             </form>
